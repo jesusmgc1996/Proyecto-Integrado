bf24cc5a0e1733688b59ebc222b8045679213045
Jesús <jesusmgc1996@gmail.com>
2024-06-18 18:32:37 +0200

    terminos legales

---
+++ resources/views/legal.blade.php
@@ -0,0 +1,73 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="mb-5 text-5xl text-center font-bold tracking-tight text-gray-900">Términos y Condiciones</div>
+        <div class="grid grid-col-1">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="mb-5 block max-w-sm p-6">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">1. Introducción</h5>
+                    <p class="font-normal text-gray-900">Bienvenido a Centro Nerine. Estos Términos y Condiciones
+                        regulan el uso de nuestro sitio web. Al acceder y usar nuestro Sitio, aceptas cumplir y estar
+                        sujeto a estos Términos. Si no estás de acuerdo con alguna parte de estos Términos, no debes
+                        usar nuestro Sitio.
+                    </p>
+                </div>
+            </div>
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="mb-5 block max-w-sm p-6">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">2. Uso del Sitio</h5>
+                    <p class="font-normal text-gray-900">El acceso al Sitio es gratuito. Sin embargo, el Centro se
+                        reserva el derecho de restringir el acceso a ciertas áreas del Sitio o a todo el Sitio a su
+                        discreción.</p>
+                    <p class="font-normal text-gray-900">El Usuario se compromete a utilizar el Sitio únicamente con
+                        fines legales y de manera que no infrinja los derechos de terceros ni restrinja o impida su uso
+                        y disfrute del Sitio.</p>
+                </div>
+            </div>
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="mb-5 block max-w-sm p-6">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">3. Privacidad y Protección de Datos
+                    </h5>
+                    <p class="font-normal text-gray-900">El uso del Sitio también se rige por nuestra Política de
+                        Privacidad. Al usar el Sitio, consientes el tratamiento de tus datos personales conforme a dicha
+                        política.</p>
+                    <p class="font-normal text-gray-900">El Centro se compromete a mantener la confidencialidad de los
+                        datos personales de los Usuarios y a utilizar medidas de seguridad para protegerlos contra
+                        accesos no autorizados.</p>
+                </div>
+            </div>
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="mb-5 block max-w-sm p-6">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">4. Limitación de Responsabilidad
+                    </h5>
+                    <p class="font-normal text-gray-900">El Centro se esfuerza por mantener la información del Sitio
+                        actualizada y precisa, pero no garantiza la exactitud, integridad o actualidad de dicha
+                        información. El uso del Sitio es bajo el propio riesgo del Usuario.</p>
+                    <p class="font-normal text-gray-900">El Sitio puede contener enlaces a otros sitios web. El Centro
+                        no se responsabiliza del contenido ni de las políticas de privacidad de dichos sitios.</p>
+                    <p class="font-normal text-gray-900">En la medida permitida por la ley, el Centro no será
+                        responsable por cualquier daño directo, indirecto, incidental, especial, consecuencial o
+                        ejemplar que resulte del uso o la imposibilidad de uso del Sitio.</p>
+                </div>
+            </div>
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="mb-5 block max-w-sm p-6">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">5. Modificaciones de los Términos
+                    </h5>
+                    <p class="font-normal text-gray-900">El Centro se reserva el derecho de modificar estos Términos en
+                        cualquier momento. Cualquier modificación será efectiva inmediatamente después de su publicación
+                        en el Sitio. El uso continuado del Sitio tras la publicación de cambios constituye la aceptación
+                        de dichos cambios.</p>
+                </div>
+            </div>
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="mb-5 block max-w-sm p-6">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">6. Ley Aplicable y Jurisdicción
+                    </h5>
+                    <p class="font-normal text-gray-900">Estos Términos se rigen por las leyes de España. Cualquier
+                        disputa relacionada con estos Términos se someterá a la jurisdicción exclusiva de los tribunales
+                        de España.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
